c236f721dfa2849efc0236cf40fa2db6ab07aa81
Kristian Stafford Smith <kristianss7890@gmail.com>
2019-04-02 15:15:46 +0100

    Added a timer for turning the ghosts blue for ten seconds, pacman cannot be killed until this timer runs out, still need to include code for pacman killing ghosts during this timer

--- SEI-P1/app.js
+++ SEI-P1/app.js
@@ -7,6 +7,7 @@ let gameInPlay = true
 let charDirection = 'right'
 let currentStep = 0
 let ghostsBlue = false
+let blueCount = 0
 let score = 0
 let bigDots = [width * 4 + 5, width * 4 + 13, width * (width - 4) + 5, width * (width - 3) - 5, width + 2]
 let livesCount = 3
@@ -15,7 +16,6 @@ const leftKey = 37
 const upKey = 38
 const rightKey = 39
 const downKey = 40
-
 
 //refactored function creating the board
 function createBoard() {
@@ -46,8 +46,6 @@ function ghostKillPacmanCheck() {
     }
   })
 }
-
-
 
 //Base class for creating the characters, everything within this class is shared by pacman and the ghosts
 class Character {
@@ -88,6 +86,7 @@ class Character {
     clearInterval(this.intervalId)
   }
 
+  //initialize empty functions for character dependent functions, functions defined below
   moveStyle() {}
   eatDotsAndGhosts() {}
 
@@ -121,12 +120,6 @@ class Pacman extends Character {
           break
       }
 
-      // moveStyle() {
-      //   currentStep = currentStep === 3 ? 0 : currentStep + 1
-      //   squares[this.index].setAttribute('data-step', currentStep)
-      //   squares[this.index].setAttribute('data-direction', charDirection)
-      // }
-
       this.move()
     })
 
@@ -143,14 +136,21 @@ class Pacman extends Character {
       if(squares[this.index].classList.contains('dots')) {
         squares[this.index].classList.remove('dots')
         score++
-        //console.log(score)
+        console.log(score)
       }
       //eat the big dots and increase the score by 50 set the ghostsBlue to be true
       if(squares[this.index].classList.contains('big-dots')) {
         squares[this.index].classList.remove('big-dots')
         score += 50
-        //turn the ghosts blue when the big dots are eaten
-        //ghostsBlue = true
+        //turn the ghosts blue when the big dots are eaten, for ten seconds
+        let intervalBlue = setInterval(() => {
+          ghostsBlue = true
+          blueCount++
+          if(blueCount === 10) {
+            clearInterval(intervalBlue)
+            ghostsBlue = false
+        }}, 1000)
+
       }
     }
 }
@@ -185,19 +185,9 @@ const ghosts = [
   new Ghost('red', 'ghost', 240)
 ]
 
-//set an interval on a smaller increment to check if each ghost space contains pacman and the ghost, then kill pacman and stop the game
+//set an interval on a small increment, calling the ghostKillPacmanCheck function
 let checkLoseInterval = setInterval(() => {ghostKillPacmanCheck()}, 5)
 
-// function ghostKillPacmanCheck() {
-//     ghosts.forEach(ghost => {
-//       if(squares[ghost.index].classList.contains('pacman') && !ghostsBlue) {
-//         //squares.forEach(index => squares[index].classList.remove('pacman'))
-//         clearInterval(checkLoseInterval)
-//         gameInPlay = false
-//         console.log('you lose')
-//     }
-//   })
-// }
 
 
 
