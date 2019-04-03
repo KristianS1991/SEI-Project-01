8e8865e8c602b4e67c7626f546d1322438f833c2
Kristian Stafford Smith <kristianss7890@gmail.com>
2019-04-03 15:48:37 +0100

    Maze array has been added and not tested, needs to be put through a for loop and add class wall to each index

--- SEI-P1/app.js
+++ SEI-P1/app.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 const width = 20
 const board = document.querySelector('.board')
 const squares = []
+const wall = [42, 44, 45, 46, 48, 49, 50, 51, 53, 54, 55, 57, 82, 84, 85, 86, 87, 89, 90, 92, 93, 94, 95, 97, 102, 104, 115, 117, 122, 124, 126, 127, 128, 129, 130, 131, 132, 133, 135, 137, 142, 144, 155, 157, 162, 166, 168, 171, 173, 177, 182, 183, 186, 188, 189, 190, 191, 193, 196, 197, 206, 213, 221, 223, 226, 228, 229, 230, 231, 233, 236, 238, 241, 243, 249, 250, 256, 258, 266, 267, 269, 270, 272, 273, 282, 283, 284, 286, 287, 292, 293, 295, 296, 297, 302, 303, 304, 306, 307, 309, 310, 312, 313, 315, 316, 317, 326, 327, 329, 330, 332, 333, 342, 343, 344, 349, 350, 355, 356, 357, 366, 367, 372, 373]
 //variable for stopping/starting the game
 let gameInPlay = true
 let charDirection = 'right'
@@ -38,6 +39,8 @@ function createBoard() {
         squares[bigDots[i]].classList.add('big-dots')
         squares[bigDots[i]].classList.remove('dots')
       }
+
+
 }
 
 //function to check if Pacman and the ghosts crosspaths, if so end the game
