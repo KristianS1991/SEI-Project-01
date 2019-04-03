89dcb0789193d6fab2474e5117a178a790b39e6a
Kristian Stafford Smith <kristianss7890@gmail.com>
2019-04-03 16:52:40 +0100

    Ghosts now do not run into eachother

--- SEI-P1/app.js
+++ SEI-P1/app.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 const width = 20
 const board = document.querySelector('.board')
 const squares = []
-const walls = [42, 44, 45, 46, 48, 49, 50, 51, 53, 54, 55, 57, 82, 84, 85, 86, 87, 89, 90, 92, 93, 94, 95, 97, 102, 104, 115, 117, 122, 124, 126, 127, 128, 129, 130, 131, 132, 133, 135, 137, 142, 144, 155, 157, 162, 166, 168, 171, 173, 177, 182, 183, 186, 188, 191, 193, 196, 197, 206, 213, 221, 223, 226, 228, 229, 230, 231, 233, 236, 238, 241, 243, 249, 250, 256, 258, 266, 267, 269, 270, 272, 273, 282, 283, 284, 286, 287, 292, 293, 295, 296, 297, 302, 303, 304, 306, 307, 309, 310, 312, 313, 315, 316, 317, 326, 327, 329, 330, 332, 333, 342, 343, 344, 349, 350, 355, 356, 357, 366, 367, 372, 373]
+const walls = [42, 44, 45, 46, 48, 49, 50, 51, 53, 54, 55, 57, 82, 84, 85, 86, 87, 89, 90, 92, 93, 94, 95, 97, 102, 104, 115, 117, 122, 124, 126, 127, 128, 129, 130, 131, 132, 133, 135, 137, 142, 144, 155, 157, 162, 166, 168, 171, 173, 177, 182, 183, 185, 186, 188, 191, 193, 194, 196, 197, 205, 206, 213, 214, 221, 223, 225, 226, 228, 229, 230, 231, 233, 234, 236, 238, 241, 243, 249, 250, 256, 258, 266, 267, 269, 270, 272, 273, 282, 283, 284, 286, 287, 292, 293, 295, 296, 297, 302, 303, 304, 306, 307, 309, 310, 312, 313, 315, 316, 317, 326, 327, 329, 330, 332, 333, 342, 343, 344, 349, 350, 355, 356, 357, 366, 367, 372, 373]
 //variable for stopping/starting the game
 let gameInPlay = true
 let charDirection = 'right'
@@ -14,7 +14,7 @@ let currentStep = 0
 let intervalBlue = 0
 //variables for score dots and lives
 let score = 0
-let bigDots = [63, 78, 149, 214, 301, 369, 378]
+let bigDots = [63, 78, 149, 215, 301, 369, 378]
 let livesCount = 3
 //initilize variables for each arrow key
 const leftKey = 37
@@ -115,7 +115,7 @@ class Character {
     if(!gameInPlay) return false
 
     this.intervalId = setInterval(() => {
-      if(squares[this.index + this.direction].classList.contains('wall')) {
+      if(squares[this.index + this.direction].classList.contains('wall') || squares[this.index + this.direction].classList.contains('ghost') ) {
         this.stopMove()
         return false
       }
